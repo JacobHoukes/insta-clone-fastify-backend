d3c6e091e1d887961195664a7c550d5d2084d909
Jacob <jayhoukes@gmail.com>
2025-12-06 14:20:13 +0100

    server adjustments

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,23 @@
 import Fastify from "fastify"
+import multipart from "@fastify/multipart"
+import fastifyStatic from "@fastify/static"
+import path from "path"
 import { databasePlugin } from "./core/database/database.plugin"
 import { postsRoutes } from "./modules/posts/posts.routes"
 import { reelsRoutes } from "./modules/reels/reels.routes"
 import { taggedRoutes } from "./modules/tagged/tagged.routes"
 import { highlightsRoutes } from "./modules/highlights/highlights.routes"
-import multipart from "@fastify/multipart"
-import path from "path"
-import fs from "fs/promises"
-import { randomUUID } from "crypto"
 
 const fastify = Fastify({
     logger: true,
 })
 
 fastify.register(multipart)
+
+fastify.register(fastifyStatic, {
+    root: path.join(process.cwd(), "public"),
+    prefix: "/",
+})
 
 fastify.register(databasePlugin)
 fastify.register(postsRoutes)
